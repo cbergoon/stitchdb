7669bfee108f5a6d3997d838e781fa9eb54a05e8
cbergoon <cbergoon@gmail.com>
2017-02-11 15:56:06 -0500

    Simple driver func to test; still need to make db in separate package

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
-  
-  fmt.Println("Empty project...")
+func main() {
+
+	c, _ := NewConfig(Persist, DirPath("path/to/loc/"), Sync(MNGFREQ), ManageFrequency(1*time.Second), Developer, PerformanceMonitor, BucketFileMultLimit(10))
+	s, _ := NewStitchDB(c)
+	s.Open()
+	//opts, _ := NewBucketOptions(BTreeDegree(32))
+	//s.CreateBucket("test", opts)
+	//s.CreateBucket("test1", opts)
+	//s.CreateBucket("test2", opts)
+	//s.DropBucket("test1")
+	fmt.Println(s)
 
 }
