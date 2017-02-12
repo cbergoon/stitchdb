b4fd2a937f7ccd7fac7be7d5893cbf653bc904f5
cbergoon <cbergoon@gmail.com>
2017-02-12 16:32:36 -0500

    Adding entry type/options

--- entry.go
+++ entry.go
@@ -1,5 +1,21 @@
 package main
 
+import "github.com/cbergoon/btree"
+
 type Entry struct {
-	//implements btree.Item
+	k    string
+	v    string
+	opts *EntryOptions
+}
+
+func NewEntry(k string, v string, options *EntryOptions) (*Entry, error) {
+	return &Entry{
+		k:    k,
+		v:    v,
+		opts: options,
+	}, nil
+}
+
+func (e *Entry) Less(than btree.Item, itype interface{}) bool {
+	return false
 }
--- entry_options.go
+++ entry_options.go
@@ -1,2 +1,33 @@
 package main
 
+import "time"
+
+type EntryOptions struct {
+	expTime time.Time
+	invTime time.Time
+}
+
+func ExpireTime(time time.Time) func(*EntryOptions) error {
+	return func(e *EntryOptions) error {
+		e.expTime = time
+		return nil
+	}
+}
+
+func InvalidTime(time time.Time) func(*EntryOptions) error {
+	return func(e *EntryOptions) error {
+		e.invTime = time
+		return nil
+	}
+}
+
+func NewEntryOptions(options ...func(*EntryOptions) error) (*EntryOptions, error) {
+	c := &EntryOptions{}
+	for _, option := range options {
+		err := option(c)
+		if err != nil {
+			return nil, ErrCouldNotCreateConfig
+		}
+	}
+	return c, nil
+}
